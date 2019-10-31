72a0345346b21e79d2a41e11b32c9f44cc1e7893
machida <machida@kmc.gr.jp>
2019-11-01 00:18:52 +0900

    過去2週間のプレイ時間を表示する#twoコマンド

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require 'discordrb'
 require 'dotenv'
+require 'steam-api'
 
 Dotenv.load
 
@@ -9,11 +10,19 @@ pp ENV["CLIENT_ID"]
 bot = Discordrb::Commands::CommandBot.new(
   token: ENV["TOKEN"],
   client_id: ENV["CLIENT_ID"],
-  prefix:'/',
+  prefix:'#',
 )
 
 bot.command :hello do |event|
   event.send_message("hello,world.#{event.user.name}")
+end
+
+bot.command :two do |event|
+  Steam.apikey = ENV["STEAM_API_KEY"]
+  data = Steam::Player.recently_played_games(ENV["STEAM_ID"])["games"]
+  sum_of_playtime_2weeks = data.inject(0){ |sum, d| sum + d["playtime_2weeks"]}
+  hour, minute = sum_of_playtime_2weeks.divmod(60)
+  event.send_message("#{hour}時間#{minute}分")
 end
 
 bot.run
