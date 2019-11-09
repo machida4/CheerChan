c7f6a5556d9e75c1dc4825297e451f1278c382a0
machida <machida@kmc.gr.jp>
2019-11-09 18:54:54 +0900

    もうわけわからんくなってきた

--- main.rb
+++ main.rb
@@ -2,64 +2,23 @@ require 'discordrb'
 require 'dotenv'
 require 'steam-api'
 require 'active_record'
+require 'date'
+
 require_relative 'user'
 require_relative 'playtime'
-require 'date'
+require_relative 'meubot'
 
 Dotenv.load
 
 Steam.apikey = ENV["STEAM_API_KEY"]
 
-bot = Discordrb::Commands::CommandBot.new(
+command_bot = Discordrb::Commands::CommandBot.new(
   token: ENV["TOKEN"],
   client_id: ENV["CLIENT_ID"],
   prefix:'meu ',
 )
-previous = Date.today
 
-bot.heartbeat do |event|
-  now = Date.today
-  next unless previous < now
-  previous = now
-
-  User.all.each do |user|
-    data = Steam::Player.owned_games(user.steamid, params: {include_appinfo:true, include_played_free_games:true})["games"]
-    current_games = {}
-    data.each do |d|
-      next if d["playtime_forever"] == 0
-      current_games[d["appid"]] = {name: d["name"], playtime_forever: d["playtime_forever"]}
-    end
-    unless Playtime.where(steamid: user.steamid).present?
-      current_playtime = Playtime.new(steamid: user.steamid, game_playtime_hash: current_games)
-      current_playtime.save!
-      next
-    end
-    previous_games = Playtime.where(steamid: user.steamid).order(created_at: :desc).take.game_playtime_hash
-    current_playtime = Playtime.new(steamid: user.steamid, game_playtime_hash: current_games)
-    current_playtime.save!
-    detail = ""
-    sum_of_playtime = 0
-    current_games.each do |appid, hash|
-      if !previous_games.key? appid
-        diff = hash[:playtime_forever]
-      else
-        diff = hash[:playtime_forever] - previous_games[appid][:playtime_forever]
-      end
-      if diff == 0
-        next
-      else
-        hour, minute = diff.divmod(60)[0], diff.divmod(60)[1]
-        sum_of_playtime += diff
-        detail << "--**#{hash[:name]}**\n"
-        detail << "----#{hour.to_s.rjust(2, '0')}時間#{minute.to_s.rjust(2, '0')}分\n"
-      end
-    end
-    sum_hour, sum_minute = sum_of_playtime.divmod(60)[0], sum_of_playtime.divmod(60)[1]
-    message = "**#{user.name}** の今日のプレイ時間は **#{sum_hour.to_s.rjust(2, '0')}**時間**#{sum_minute.to_s.rjust(2, '0')}分** めう！\n" + detail
-    bot.send_message(ENV["CHANNEL_ID"], message)
-    sleep(2)
-  end
-end
+bot = MeuBot.new(command_bot)
 
 bot.command :hello do |event|
   "hello, #{event.user.id}!"
